5154fa4de39f660bdbd6b4be8b81d11507da367e
Roman Mykhasiv <mihasivroman@gmail.com>
2022-11-16 19:42:09 +0200

    feature: set up webpack for css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Post from "./Post";
+import Cubes from './assets/cubes.png'
+import json from './assets/json.json'
 import './styles/styles.css'
 
-const post = new Post('Webpack Post Title')
+const post = new Post('Webpack Post Title', Cubes)
 
 console.log('Post to String:', post.toString())
+
+console.log('JSON:', json)
