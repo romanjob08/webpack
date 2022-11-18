ddef7c57c7a892fed6d0169436a21814ce0c4940
Roman Mykhasiv <mihasivroman@gmail.com>
2022-11-18 20:51:00 +0200

    feature: added webpack settings for css less sass

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as $ from 'jquery'
 import xml from './assets/data.xml'
 import json from './assets/json'
 import './styles/styles.css'
+import './styles/less.less'
+import './styles/scss.scss'
 
 const post = new Post('Webpack Post Title')
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,46 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
+
+const fileName = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+    const loaders = [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+            hmr: isDev,
+            reloadAll: true
+        },
+    },
+        'css-loader'
+    ]
+
+    if (extra) {
+        loaders.push(extra)
+    }
+    return loaders
+}
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
@@ -14,7 +50,7 @@ module.exports = {
         analytics: './analytics.js',
     },
     output: {
-        filename: '[name].[contenthash].js',
+        filename: fileName('js'),
         path: path.resolve(__dirname, 'dist')
     },
     resolve: {
@@ -24,11 +60,7 @@ module.exports = {
             '@': path.resolve(__dirname, 'src'),
         }
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
     devServer: {
         port: 4200,
         hot: isDev
@@ -42,20 +74,22 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
+            filename: fileName('css')
         })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        hmr: isDev,
-                        reloadAll: true
-                    },
-                }, 'css-loader']
+                use: cssLoaders()
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
             },
             // {
             //     test: /\.(png|jpeg|svg|gif)$/,
